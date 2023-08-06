983a5bb5334b6cb08e350cb9d94a9007bd7199d5
Alice <alice_hillier@hotmail.com>
2023-08-06 18:35:20 +0100

    write 'start' function, including user input and conditional statement for dealing with different answers

--- run.py
+++ run.py
@@ -2,7 +2,27 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-print("The winter is colder than usual. We haven’t had snow in years, but now it envelops everything in sight.")
-print("The birds are long gone, and even the rats that swarmed the street in summer are either in hiding or dead.")
-print("Webs of ice have gathered on the windows and I can barely make out the shapes of cars under snowy rubble.")
-print("I’m tired, but I know it must be done today.")
+def start():
+    print("The winter is colder than usual. We haven’t had snow in years, but now it envelops everything in sight.")
+    print("The birds are long gone, and even the rats that swarmed the street in summer are either in hiding or dead.")
+    print("Webs of ice have gathered on the windows and I can barely make out the shapes of cars under snowy rubble.")
+    print("I’m tired, but I know it must be done today.\n")
+    print("Enter 1 or 2.\n")
+    print("1. Do it.\n2. Delay it.\n")
+    decide = input('')
+
+    if int(decide) == 1:
+        do_it()
+    elif int(decide) == 2:
+        delay_it()
+    else:
+        print('Please enter 1 or 2.')
+        start()
+
+def do_it():
+    print('option 1')
+    
+def delay_it():
+    print('option 2')
+
+start()

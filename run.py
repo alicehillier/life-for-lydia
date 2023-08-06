4498238b8f2210ef31fe799964a02a9d95b15677
Alice <alice_hillier@hotmail.com>
2023-08-06 18:20:24 +0100

    initial commit. print opening lines of story

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print("The winter is colder than usual. We haven’t had snow in years, but now it envelops everything in sight.")
+print("The birds are long gone, and even the rats that swarmed the street in summer are either in hiding or dead.")
+print("Webs of ice have gathered on the windows and I can barely make out the shapes of cars under snowy rubble.")
+print("I’m tired, but I know it must be done today.")

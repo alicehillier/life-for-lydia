27742a0069d9e12e279dbdd50cc2e1437a96c1ea
Alice <alice_hillier@hotmail.com>
2023-08-08 17:34:40 +0100

    copy code from other functions into third and fourth decision functions, moving the story along

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# Each dictionary contains information about the story selected.
 lydia = {
     "Protagonist": "Lydia",
     "Title": "The Morning After",
@@ -17,6 +18,10 @@ anita = {
     }
 
 def story_selector():
+    """
+    Asks the user to select a story. When the player does so, they are able to view general information about the story 
+    before confirming their selection. Once confirmed, the relevant story will play out.
+    """
     print("Enter 1, 2 or 3 to view the corresponding story's plot:\n")
     print("1. Lydia's Story.\n2. Simon's Story.\n3. Anita's story.")
     while True:
@@ -54,9 +59,9 @@ def story_selector():
                         story_selector()
         except ValueError as e:
             if story_selected.isalpha():
-                print(f"You entered {story_selected}. That's not a number! Please enter 1, 2 or 3!\n")
+                print(f"You entered {story_selected}. That's not a number! Please enter a valid number from the options above!\n")
             else:
-                print(f"You entered {story_selected}. Please enter 1, 2 or 3!\n")
+                print(f"You entered {story_selected}. Please enter a valid number from the options above!\n")
             continue
 
 name = story_selector()
@@ -174,5 +179,72 @@ def clean_up():
     """
     print("I start by clearing the area around the bed, kicking clothes to the side to clear a path to the bed.") 
     print("I’ll need something to clear up the glass.")
+    third_decision()
 
-    start()
+def third_decision():
+    """
+    Processes the user's input, calling the relevant function in accordance with the user's selection.
+    Raises ValueError if the user enters an invalid value and loops the input requirement until the value
+    is considered valid.
+    """
+    print("Enter 1 or 2.\n")
+    while True:
+        decide = input('1. Get a mop.\n2. Get a dustpan and brush.\n')
+        try:
+            if int(decide) == 1:
+                get_a_mop_1()
+                break
+            elif int(decide) == 2:
+                get_a_dustpan()
+                break
+            else:
+                raise ValueError
+        except ValueError as e:
+            if decide.isalpha():
+                print(f"You entered {decide}. That's not a number! Please enter 1 or 2!\n")
+            else:
+                print(f"You entered {decide}. Please enter 1 or 2!\n")
+            continue
+
+def get_a_mop_1():
+    print("I don’t think a mop is the best choice here.")
+    print("1. Get a dustpan and brush.\n")
+    wrong_decision()
+    get_a_dustpan()
+
+def get_a_dustpan():
+    print("I retrieve the dustpan and brush from the cupboard under the kitchen sink and begin to clear up the glass.") 
+    print("Now that’s done, I need to get rid of all this blood.")
+    fourth_decision()
+
+def fourth_decision():
+    """
+    Processes the user's input, calling the relevant function in accordance with the user's selection.
+    Raises ValueError if the user enters an invalid value and loops the input requirement until the value
+    is considered valid.
+    """
+    print("Enter 1 or 2.\n")
+    while True:
+        decide = input('1. Get tissues.\n2. Get towels.\n')
+        try:
+            if int(decide) == 1:
+                get_tissues()
+                break
+            elif int(decide) == 2:
+                get_towels()
+                break
+            else:
+                raise ValueError
+        except ValueError as e:
+            if decide.isalpha():
+                print(f"You entered {decide}. That's not a number! Please enter 1 or 2!\n")
+            else:
+                print(f"You entered {decide}. Please enter 1 or 2!\n")
+            continue
+
+def get_tissues():
+    print('get tissues now')
+def get_towels():
+    print('get towels now')
+
+start()

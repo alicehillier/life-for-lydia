d626fbcff7e08d576daad23c56d2805bea5b17ea
Alice <alice_hillier@hotmail.com>
2023-08-15 14:52:38 +0100

    reduce number of print statements in chapter1.py and break up the dialogue over several lines for clarity

--- chapter1.py
+++ chapter1.py
@@ -22,10 +22,11 @@ def chapter1():
     """
     Prints the opening lines of the story and calls for the user to make their first decision.
     """
-    print("The winter is colder than usual. We haven’t had snow in years, but now it envelops everything in sight.")
-    print("The birds are long gone, and even the rats that swarmed the street in summer are either in hiding or dead.")
-    print("Webs of ice have gathered on the windows and I can barely make out the shapes of cars under snowy rubble.")
-    print("I’m tired, but I know it must be done today.\n")
+    print("The winter is colder than usual. We haven’t had snow in years, but\n"
+    "now it envelops everything in sight. The birds are long gone, and even the\n"
+    "rats that swarmed the street in summer are either in hiding or dead. Webs\n"
+    "of ice have gathered on the windows and I can barely make out the shapes\n"
+    "of cars under snowy rubble. I’m tired, but I know it must be done today.\n")
     first_decision()
     
 def first_decision():
@@ -34,7 +35,7 @@ def first_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1 or 2.\n")
+    print("\nEnter 1 or 2.\n")
     while True:
         decide = input('1. Do it.\n2. Delay it.\n')
         try:
@@ -55,9 +56,10 @@ def do_it():
     Runs if the user selects the first option in the start function. 
     Prints lines of the story and calls for the user to make the next decision.
     """
-    print("I slowly climb the stairs, my hand slipping as I try to hold onto the rail.")
-    print("I tighten my grip and continue upwards. As I turn to the bedroom, I take a deep breath.")
-    print("Standing in the doorway, I look at him. I look at the mess I’ve made.")
+    print("I slowly climb the stairs, my hand slipping as I try to hold onto the\n"
+    "rail. I tighten my grip and continue upwards. As I turn to the bedroom, I\n"
+    "take a deep breath. Standing in the doorway, I look at him. I look at the\n"
+    "mess I’ve made.\n")
     second_decision()
 
 def second_decision():
@@ -66,7 +68,7 @@ def second_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1 or 2.\n")
+    print("\nEnter 1 or 2.\n")
     while True:
         decide = input('1. Try to wake him.\n2. Clean up.\n')
         try:
@@ -97,7 +99,7 @@ def try_to_wake_him():
     Runs if the user selects the first option in the do_it function. 
     Prints lines of the story and calls for the user to make the next decision.
     """
-    print("At this point, it seems pointless to try to wake him up.")
+    print("At this point, it seems pointless to try to wake him up.\n")
     print("1. Clean up.\n")
     wrong_decision()
     clean_up()
@@ -107,8 +109,8 @@ def clean_up():
     Runs if the user selects the second option in the do_it function. 
     Prints lines of the story and calls for the user to make the next decision.
     """
-    print("I start by clearing the area around the bed, kicking clothes to the side to clear a path to the bed.") 
-    print("I’ll need something to clear up the glass.")
+    print("I start by clearing the area around the bed, kicking clothes to the\n"
+    "side to clear a path. I'll need something to clear up the glass.\n")
     third_decision()
 
 def third_decision():
@@ -117,7 +119,7 @@ def third_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1 or 2.\n")
+    print("\nEnter 1 or 2.\n")
     while True:
         decide = input('1. Get a mop.\n2. Get a dustpan and brush.\n')
         try:
@@ -134,14 +136,15 @@ def third_decision():
             continue
 
 def get_a_mop_1():
-    print("I don’t think a mop is the best choice here.")
+    print("I don’t think a mop is the best choice here.\n")
     print("1. Get a dustpan and brush.\n")
     wrong_decision()
     get_a_dustpan()
 
 def get_a_dustpan():
-    print("I retrieve the dustpan and brush from the cupboard under the kitchen sink and begin to clear up the glass.") 
-    print("Now that’s done, I need to get rid of all this blood.")
+    print("I retrieve the dustpan and brush from the cupboard under the kitchen\n"
+    "sink and begin to clear up the glass. Now that’s done, I need to get rid of\n"
+    "all this blood.\n")
     fourth_decision()
 
 def fourth_decision():
@@ -150,7 +153,7 @@ def fourth_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1 or 2.\n")
+    print("\nEnter 1 or 2.\n")
     while True:
         decide = input('1. Get tissues.\n2. Get towels.\n')
         try:
@@ -173,7 +176,8 @@ def get_tissues():
     get_towels()
 
 def get_towels():
-    print("It takes a few towels, but they soak up most of the blood. I’ll have to use a mop for the rest.\n")
+    print("It takes a few towels, but they soak up most of the blood. I’ll have\n"
+    "to use a mop for the rest.\n")
     fifth_decision()
 
 def fifth_decision():
@@ -182,7 +186,7 @@ def fifth_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1.\n")
+    print("\nEnter 1.\n")
     while True:
         decide = input('1. Get a mop.\n')
         try:
@@ -196,8 +200,9 @@ def fifth_decision():
             continue
 
 def get_a_mop_2():
-    print("As I make my way downstairs, I hear a barrage of heavy bangs on the front door.") 
-    print("I look down at my clothes. I can’t see any stains. Should I open the door?")
+    print("As I make my way downstairs, I hear a barrage of heavy bangs on the\n"
+    "front door. I look down at my clothes. I can’t see any stains. Should I\n"
+    "open the door?\n")
     sixth_decision()
 
 def sixth_decision():
@@ -206,7 +211,7 @@ def sixth_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1 or 2.\n")
+    print("\nEnter 1 or 2.\n")
     while True:
         decide = input('1. Open the door.\n2. Ignore it.\n')
         try:
@@ -223,7 +228,7 @@ def sixth_decision():
             continue
 
 def open_the_door():
-    print("Are you sure?")
+    print("Are you sure?\n")
     seventh_decision()
 
 def seventh_decision():
@@ -232,7 +237,7 @@ def seventh_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1 or 2.\n")
+    print("\nEnter 1 or 2.\n")
     while True:
         decide = input('1. Look for stains.\n2. Open it.\n')
         try:
@@ -249,33 +254,32 @@ def seventh_decision():
             continue
 
 def ignore_it():
-    print("I continue downstairs, quickly and quietly.") 
-    print("As I go to open the storage cupboard under the stairs, I notice blood on my hands.")
-    print("I rush back to the stairs and check the bannister.")
-    print("Long streaks of blood have stained the white-painted wood.")
-    print("That’s why my hand slipped earlier…")
-    print("I hear the squeak of metal and look at the door.")
-    print("The letterbox is open and dark eyes glare at me.")
+    print("I continue downstairs, quickly and quietly. As I go to open the\n"
+    "storage cupboard under the stairs, I notice blood on my hands. I rush back\n"
+    "to the stairs and check the bannister. Long streaks of blood have stained\n"
+    "the white-painted wood. That’s why my hand slipped earlier...\n"
+    "I hear the squeak of metal and look at the door. The letterbox is open and\n"
+    "dark eyes glare at me.\n")
     ninth_decision()
 
 def look_for_stains():
-    print("I check my clothes again and see some splashes of blood on my slippers.")
-    print("As I pull my gown towards me to get a better look, the fabric sticks to my hands.")
-    print("I pry them away and see that they are still wet with blood.")
-    print("I hear the squeak of metal and look at the door.")
-    print("The letterbox is open and dark eyes glare at me as I stand on the stairs.")
-    print("'We are responding to a call. Open the door, please,' the man says")
+    print("I check my clothes again and see some splashes of blood on my\n"
+    "slippers. As I pull my gown towards me to get a better look, the fabric\n"
+    "sticks to my hands. I pry them away and see that they are still wet with\n"
+    "blood. I hear the squeak of metal and look at the door. The letterbox is\n"
+    "open and dark eyes glare at me as I stand on the stairs.\n"
+    "'We\'re responding to a call. Open the door, ma\'am,' the man says.\n")
     ninth_decision()
 
 def open_it():
-    print("I try to compose myself and open the door with a soft smile.")
-    print("In front of me are two police officers.")
-    print("'Miss?' the older man says, his brows furrowed.") 
-    print("The younger officer is staring at my hands.")
-    print("I look down at them and see that they are still wet with blood.")
-    print("'Can we come in?'”' The older man asks.")
-    print("I try to think of a response, but my mind is blank.")
-    print("I feel as though my heart has stopped. The man steps towards me.")
+    print("I try to compose myself and open the door with a soft smile. In front\n"
+    "of me are two police officers.\n"
+    "'Ma\'am?' the older man says, his brows furrowed.\n"
+    "The younger officer is staring at my hands. I look down at them and see\n"
+    "that they are still wet with blood.\n"
+    "'Can we come in?'”' The older man asks.\n"
+    "I try to think of a response, but my mind is blank. I feel as though my\n"
+    "heart has stopped. The man steps towards me.\n")
     eighth_decision()
 
 def eighth_decision():
@@ -284,7 +288,7 @@ def eighth_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1 or 2.\n")
+    print("\nEnter 1 or 2.\n")
     while True:
         decide = input('1. Think of an excuse.\n')
         try:
@@ -298,9 +302,9 @@ def eighth_decision():
             continue
 
 def think_of_excuse():
-    print("'I… sorry, I was just about to go out,' I say.")
-    print("'In your dressing gown, Miss?' the older man says.")
-    print("I slam the door shut.")
+    print("'I... sorry, I was just about to go out,' I say.\n"
+    "'In your dressing gown, Miss?' the older officer says.\n"
+    "I slam the door shut.\n")
     ninth_decision()
 
 def ninth_decision():
@@ -309,7 +313,7 @@ def ninth_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1.\n")
+    print("\nEnter 1.\n")
     while True:
         decide = input('1. Escape.\n')
         try:
@@ -323,9 +327,9 @@ def ninth_decision():
             continue
 
 def escape():
-    print("I rush to the kitchen to escape out the back door.")
-    print("As I trudge through the thick snow, my slippers drenched and my feet freezing, I reach the back alley.")
-    print("I hear men shouting something at me.")
+    print("I rush to the kitchen to escape out the back door. As I trudge\n"
+    "through the thick snow, my slippers drenched and my feet freezing, I reach\n"
+    "the back alley. I hear the officers shouting something at me.\n")
     tenth_decision()
 
 def tenth_decision():
@@ -334,7 +338,7 @@ def tenth_decision():
     Raises ValueError if the user enters an invalid value and loops the input requirement until the value
     is considered valid.
     """
-    print("Enter 1 or 2.\n")
+    print("\nEnter 1 or 2.\n")
     while True:
         decide = input('1. Surrender.\n2. Keep running.\n')
         try:
@@ -351,17 +355,18 @@ def tenth_decision():
             continue
 
 def surrender():
-    print("I look to my left at the end of the alley and see the officer rushing towards me.")
-    print("'Stop!' he yells. I glance to my right. I know I cannot outrun him.")
-    print("I raise my hands and he approaches me.")
-    print("'Lets have a talk,' he says.")
+    print("I look to my left at the end of the alley and see an officer rushing\n"
+    "towards me."
+    "'Stop!' he yells.\n"
+    "I glance to my right. I know I cannot outrun him. I raise my hands and he\n"
+    "approaches me.\n"
+    "'Lets have a talk,' he says.\n")
 
 def keep_running():
-    print("I kick off my slippers and my feet are raw.")
-    print("I head right and follow the alley as it winds between houses.")
-    print("I glance behind me briefly and see the two officers.")
-    print("'Stop!' one of the men yells. I have to outrun them.")
-    print("As I turn a corner, something sharp cuts into my foot.")
-    print("I scream and feel myself falling forwards.")
-    print("I hear a crack as my knee hits the ground. The men stand over me.")
-    print("I’m done.")
+    print("I kick off my slippers and my feet are raw. I head right and follow\n"
+    "the alley as it winds between houses. I glance behind me briefly and see\n"
+    "the two officers.\n"
+    "'Stop!' one of the men yells.\n"
+    "I have to outrun them. As I turn a corner, something sharp cuts into my\n"
+    "foot. I scream and fall forwards, hearing a crack as my knee hits the\n"
+    "ground. The men stand over me. I'm done.\n")
